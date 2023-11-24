245fd363cbd372b351c109d06a7787bb6e28f06f
Igo Santana <igo.santana33@mgmail.com>
2023-11-24 19:12:57 -0300

    feat: mudancas ok

--- plugin/packer_compiled.lua
+++ plugin/packer_compiled.lua
@@ -89,11 +89,6 @@ _G.packer_plugins = {
     path = "/home/igosantana/.local/share/nvim/site/pack/packer/start/cmp-nvim-lsp",
     url = "https://github.com/hrsh7th/cmp-nvim-lsp"
   },
-  ["git.nvim"] = {
-    loaded = true,
-    path = "/home/igosantana/.local/share/nvim/site/pack/packer/start/git.nvim",
-    url = "https://github.com/dinhhuy258/git.nvim"
-  },
   ["gitsigns.nvim"] = {
     loaded = true,
     path = "/home/igosantana/.local/share/nvim/site/pack/packer/start/gitsigns.nvim",
